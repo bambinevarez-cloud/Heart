472c952986c26071ff83ea3dd92d300bfa08fe0b
bambinevarez-cloud <bambinevarez@gmail.com>
2026-06-01 17:13:35 -0600

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const result = document.getElementById("result");
+const container = document.querySelector(".container");
 
+noBtn.addEventListener("mouseover", () => {
+
+    const maxX =
+        container.clientWidth - noBtn.offsetWidth;
+
+    const maxY =
+        container.clientHeight - noBtn.offsetHeight;
+
+    const newX =
+        Math.floor(Math.random() * maxX);
+
+    const newY =
+        Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = `${newX}px`;
+    noBtn.style.top = `${newY}px`;
+
+});
+
+yesBtn.addEventListener("click", () => {
+
+    document.querySelector(".gif").style.display = "none";
+
+    document.querySelector("h2").style.display = "none";
+
+    yesBtn.style.display = "none";
+
+    noBtn.style.display = "none";
+
+    result.style.display = "block";
+
+});
